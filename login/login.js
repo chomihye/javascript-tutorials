72853d7dd8ddd43cda22a33db5b9daea6b6ba1ca
chomihye <devmihye@gmail.com>
2023-07-03 17:31:21 +0900

    if문 수정

--- login/login.js
+++ login/login.js
@@ -12,20 +12,23 @@ function checkField() {
         alert("아이디를 입력하세요");
         id.focus();
         return false;
-    }
-    if (pw.value.length == 0) {
+    } else if (pw.value.length == 0) {
         alert("비밀번호를 입력하세요");
         pw.focus();
-    }
-    if (id.value == "test" && pw.value == "1234") {
+        return false;
+    } else if (id.value == "test" && pw.value == "1234") {
         alert("로그인 성공");
+        console.log("로그인 성공");
+        moveUrl();
+    } else {
+        alert("아이디나 패스워드를 확인하세요");
     }
 }
 
+//네이버로 이동
 function moveUrl() {
-    let link = "www.naver.com";
+    let link = "http://www.naver.com";
     location.href = link;
 }
 
 loginBtn.addEventListener("click", checkField);
-loginBtn.addEventListener("click", moveUrl);
