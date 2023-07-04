864c8650bb1046bb1b85d786ceddae9328ba7784
chomihye <devmihye@gmail.com>
2023-07-04 17:34:49 +0900

    json 적용

--- tablecheckbox/check.js
+++ tablecheckbox/check.js
@@ -1,22 +1,54 @@
-const checkAll = document.querySelector("#checkAll");
-const checkbox = document.querySelectorAll(".check");
+//데이터
+const data = [
+    {
+        id: "abx123",
+        name: "짱구",
+        age: 19,
+    },
+    {
+        id: "zzz777",
+        name: "흰둥이",
+        age: 3,
+    },
+    {
+        id: "bwb456",
+        name: "유리",
+        age: 5,
+    },
+];
+
+//데이터 가져오기
+// td 에 내용 보여지게 하기
+data.forEach((element) => {
+    console.log(element);
+
+    const table = document.querySelector("table");
+    let row = `<tr>
+                    <td><input type="checkbox" id="checkAll" class="check" onchange="selectChecked()" /></td>
+                    <td >${element.id}</td>
+                    <td >${element.name}</td>
+                    <td >${element.age}</td>
+                </tr>`;
+    table.innerHTML += row;
+});
+
+// const checkAll = document.querySelector("#checkAll");
+// const checkbox = document.querySelectorAll(".check");
+document.querySelector("#checkAll").addEventListener("click", isChecked);
 
 //전체 체크,해제
-function isChecked() {
-    const isCheck = checkAll.checked;
+function isChecked(e) {
+    const isCheck = e.target.checked;
+    const checkbox = document.querySelectorAll(".check");
 
-    if (isCheck) {
-        for (let i = 0; i < checkbox.length; i++) {
-            checkbox[i].checked = true;
-        }
-    } else {
-        for (let i = 0; i < checkbox.length; i++) {
-            checkbox[i].checked = false;
-        }
+    for (let i = 0; i < checkbox.length; i++) {
+        checkbox[i].checked = isCheck;
     }
 }
 //개별 체크 해제
 function selectChecked() {
+    const checkAll = document.querySelector("#checkAll");
+    const checkbox = document.querySelectorAll(".check");
     let allChecked = true;
 
     for (let i = 0; i < checkbox.length; i++) {
@@ -27,8 +59,19 @@ function selectChecked() {
     checkAll.checked = allChecked;
 }
 
-checkAll.addEventListener("click", isChecked);
+// for (let i = 0; i < checkbox.length; i++) {
+//     checkbox[i].addEventListener("click", selectChecked);
+// }
 
-for (let i = 0; i < checkbox.length; i++) {
-    checkbox[i].addEventListener("click", selectChecked);
+//fetch 사용하기
+function fn() {
+    fetch("/tablecheckbox/data.json")
+        .then((res) => res.json())
+        .then((data) => {
+            data.data.forEach((a) => {
+                console.log(a);
+            });
+        });
 }
+
+fn();
