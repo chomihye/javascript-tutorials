70c5044520a16912b08152d4413345a26009c219
chomihye <devmihye@gmail.com>
2023-07-05 18:19:15 +0900

    선택 삭제

--- tablecheckbox/check.js
+++ tablecheckbox/check.js
@@ -23,11 +23,12 @@ data.forEach((element) => {
     console.log(element);
 
     const table = document.querySelector("table");
-    let row = `<tr>
+    let row = `<tr id="listBody">
                     <td><input type="checkbox" id="checkAll" class="check" onchange="selectChecked()" /></td>
                     <td >${element.id}</td>
                     <td >${element.name}</td>
                     <td >${element.age}</td>
+                    <td><button type="button" class="deleteBtn" onclick="deleteSelect()">삭제</button></td>
                 </tr>`;
     table.innerHTML += row;
 });
@@ -39,12 +40,14 @@ document.querySelector("#checkAll").addEventListener("click", isChecked);
 //전체 체크,해제
 function isChecked(e) {
     const isCheck = e.target.checked;
+    console.log(isCheck);
     const checkbox = document.querySelectorAll(".check");
 
     for (let i = 0; i < checkbox.length; i++) {
         checkbox[i].checked = isCheck;
     }
 }
+
 //개별 체크 해제
 function selectChecked() {
     const checkAll = document.querySelector("#checkAll");
@@ -59,19 +62,60 @@ function selectChecked() {
     checkAll.checked = allChecked;
 }
 
-// for (let i = 0; i < checkbox.length; i++) {
-//     checkbox[i].addEventListener("click", selectChecked);
+//fetch 사용하기
+// function fn() {
+//     fetch("/tablecheckbox/data.json")
+//         .then((res) => res.json())
+//         .then((data) => {
+//             data.data.forEach((a) => {
+//                 console.log(a);
+//             });
+//         });
 // }
 
-//fetch 사용하기
-function fn() {
-    fetch("/tablecheckbox/data.json")
-        .then((res) => res.json())
-        .then((data) => {
-            data.data.forEach((a) => {
-                console.log(a);
-            });
-        });
-}
+// fn();
 
-fn();
+// document.querySelector(".check").addEventListener("click", deleteData);
+// const deleteBtn = document.querySelector(".deleteBtn");
+
+//삭제 버튼을 누르면 체크된 게시물 삭제
+// function deleteData() {
+//     const trList = document.querySelectorAll(".dataTr");
+
+//     console.log(trList);
+
+//     if (deleted) {
+//         trList.forEach((tr) => {
+//             tr.remove();
+//         });
+//     }
+// }
+// deleteBtn.addEventListener("click", deleteData);
+
+//
+// const deleteBtn = document.querySelector(".deleteBtn");
+
+// function deleteData() {
+//     const checkbox = document.querySelectorAll(".check");
+//     const listbody = document.querySelector("#listbody");
+
+//     console.log(checkbox);
+//     for (i = 0; i < checkbox.length; i++) {
+//         if (checkbox[i].checked) {
+//             listbody.removeChild(checkbox[i].parentElement);
+//         }
+//     }
+// }
+
+// deleteBtn.addEventListener("click", deleteData);
+
+//삭제버튼 클릭시 삭제
+const deleteBtn = document.querySelector("deleteBtn");
+
+function deleteSelect() {
+    const checkbox = document.querySelectorAll(".check");
+    console.log(checkbox.checked);
+    if (checkbox.checked) {
+        console.log("asdfasfd");
+    }
+}
