d305cfc48b0784e743fb4acc1676cb9af1e15a9f
chomihye <devmihye@gmail.com>
2023-07-06 18:28:55 +0900

    오름차순 내림차순

--- tablecheckbox/check.js
+++ tablecheckbox/check.js
@@ -1,36 +1,36 @@
 //데이터
 const data = [
-  {
-    id: "abx123",
-    name: "짱구",
-    age: 19,
-  },
-  {
-    id: "zzz777",
-    name: "흰둥이",
-    age: 3,
-  },
-  {
-    id: "bwb456",
-    name: "유리",
-    age: 5,
-  },
+    {
+        id: "abx123",
+        name: "짱구",
+        age: 19,
+    },
+    {
+        id: "zzz777",
+        name: "흰둥이",
+        age: 3,
+    },
+    {
+        id: "bwb456",
+        name: "유리",
+        age: 5,
+    },
 ];
 
 //데이터 가져오기
 // td 에 내용 보여지게 하기
 data.forEach((element) => {
-  console.log(element);
+    console.log(element);
 
-  const table = document.querySelector("table");
-  let row = `<tr id="listBody">
+    const table = document.querySelector("table");
+    let row = `<tr id="listBody">
                     <td><input type="checkbox" id="checkAll" class="check" onchange="selectChecked()" /></td>
                     <td >${element.id}</td>
                     <td >${element.name}</td>
                     <td >${element.age}</td>
                     <td><button type="button" class="deleteBtn" >삭제</button></td>
                 </tr>`;
-  table.innerHTML += row;
+    table.innerHTML += row;
 });
 
 // const checkAll = document.querySelector("#checkAll");
@@ -39,27 +39,27 @@ document.querySelector("#checkAll").addEventListener("click", isChecked);
 
 //전체 체크,해제
 function isChecked(e) {
-  const isCheck = e.target.checked;
-  console.log(isCheck);
-  const checkbox = document.querySelectorAll(".check");
+    const isCheck = e.target.checked;
+    console.log(isCheck);
+    const checkbox = document.querySelectorAll(".check");
 
-  for (let i = 0; i < checkbox.length; i++) {
-    checkbox[i].checked = isCheck;
-  }
+    for (let i = 0; i < checkbox.length; i++) {
+        checkbox[i].checked = isCheck;
+    }
 }
 
 //개별 체크 해제
 function selectChecked() {
-  const checkAll = document.querySelector("#checkAll");
-  const checkbox = document.querySelectorAll(".check");
-  let allChecked = true;
+    const checkAll = document.querySelector("#checkAll");
+    const checkbox = document.querySelectorAll(".check");
+    let allChecked = true;
 
-  for (let i = 0; i < checkbox.length; i++) {
-    if (!checkbox[i].checked) {
-      allChecked = false;
+    for (let i = 0; i < checkbox.length; i++) {
+        if (!checkbox[i].checked) {
+            allChecked = false;
+        }
     }
-  }
-  checkAll.checked = allChecked;
+    checkAll.checked = allChecked;
 }
 
 //fetch 사용하기
@@ -110,18 +110,18 @@ function selectChecked() {
 // deleteBtn.addEventListener("click", deleteData);
 
 //삭제버튼 클릭시 삭제
-const deleteBtn = document.querySelector("deleteBtn");
+// const deleteBtn = document.querySelector("deleteBtn");
 
-function deleteSelect() {
-  const checkbox = document.querySelectorAll(".check");
-  console.log(checkbox.checked);
-  if (checkbox.checked) {
-    console.log("asdfasfd");
-  }
-}
+// function deleteSelect() {
+//   const checkbox = document.querySelectorAll(".check");
+//   console.log(checkbox.checked);
+//   if (checkbox.checked) {
+//     console.log("asdfasfd");
+//   }
+// }
 
-for (let i = 0; i < deleteBtn.length; i++) {
-  deleteBtn[i].addEventListener("click", function () {
-    console.log("eee");
-  });
-}
+// for (let i = 0; i < deleteBtn.length; i++) {
+//   deleteBtn[i].addEventListener("click", function () {
+//     console.log("eee");
+//   });
+// }
--- tablecheckbox/check2.js
+++ tablecheckbox/check2.js
@@ -1,44 +1,44 @@
 //데이터
 let data = [
-  {
-    id: "abx123",
-    name: "짱구",
-    age: 19,
-  },
-  {
-    id: "zzz777",
-    name: "흰둥이",
-    age: 3,
-  },
-  {
-    id: "bwb456",
-    name: "유리",
-    age: 5,
-  },
+    {
+        id: "abx123",
+        name: "짱구",
+        age: 19,
+    },
+    {
+        id: "zzz777",
+        name: "흰둥이",
+        age: 3,
+    },
+    {
+        id: "bwb456",
+        name: "유리",
+        age: 5,
+    },
 ];
 
 document.addEventListener("DOMContentLoaded", function (e) {
-  setTableData(data);
+    setTableData(data);
 
-  document.querySelector("#checkAll").addEventListener("click", isChecked);
+    document.querySelector("#checkAll").addEventListener("click", isChecked);
 
-  document
-    .querySelector(".deleteAllBtn")
-    .addEventListener("click", function (e) {
-      const checkbox = document.querySelectorAll(".check:checked");
-      checkbox.forEach((cb) => {
-        // console.log(cb.dataset.id);
-        data = data.filter((item) => item.id !== cb.dataset.id);
-      });
-      setTableData(data);
+    document.querySelector(".deleteAllBtn").addEventListener("click", function (e) {
+        const checkbox = document.querySelectorAll(".check:checked");
+        checkbox.forEach((cb) => {
+            // console.log(cb.dataset.id);
+            data = data.filter((item) => item.id !== cb.dataset.id);
+        });
+        setTableData(data);
     });
+
+    document.querySelector(".sortBtn").addEventListener("click", sortToggle);
 });
 
 function setTableData(list) {
-  const table = document.querySelector("#check_table tbody");
-  table.innerHTML = "";
-  list.forEach((element) => {
-    let row = `
+    const table = document.querySelector("#check-table tbody");
+    table.innerHTML = "";
+    list.forEach((element) => {
+        let row = `
         <tr id="listBody">
             <td><input type="checkbox" id="checkAll" class="check" data-id="${element.id}" onchange="selectChecked()" /></td>
             <td >${element.id}</td>
@@ -47,42 +47,62 @@ function setTableData(list) {
             <td><button type="button" class="deleteBtn" data-id="${element.id}">삭제</button></td>
         </tr>
     `;
-    table.innerHTML += row;
-  });
-  setDeleteButtonEvent();
+        table.innerHTML += row;
+    });
+    setDeleteButtonEvent();
 }
 
 // 개별 삭제버튼 이벤트 설정
 function setDeleteButtonEvent() {
-  const btn_list = document.querySelectorAll(".deleteBtn");
-  btn_list.forEach((btn) => {
-    btn.addEventListener("click", function (e) {
-      data = data.filter((item) => item.id !== this.dataset.id);
-      setTableData(data);
+    const btn_list = document.querySelectorAll(".deleteBtn");
+    btn_list.forEach((btn) => {
+        btn.addEventListener("click", function (e) {
+            data = data.filter((item) => item.id !== this.dataset.id);
+            setTableData(data);
+        });
     });
-  });
 }
 
 //개별 체크 해제
 function selectChecked() {
-  const checkAll = document.querySelector("#checkAll");
-  const checkbox = document.querySelectorAll(".check");
-  let allChecked = true;
+    const checkAll = document.querySelector("#checkAll");
+    const checkbox = document.querySelectorAll(".check");
+    let allChecked = true;
 
-  for (let i = 0; i < checkbox.length; i++) {
-    if (!checkbox[i].checked) {
-      allChecked = false;
+    for (let i = 0; i < checkbox.length; i++) {
+        if (!checkbox[i].checked) {
+            allChecked = false;
+        }
     }
-  }
-  checkAll.checked = allChecked;
+    checkAll.checked = allChecked;
 }
 
 //전체 체크,해제
 function isChecked(e) {
-  const isCheck = e.target.checked;
-  const checkbox = document.querySelectorAll(".check");
+    const isCheck = e.target.checked;
+    const checkbox = document.querySelectorAll(".check");
 
-  for (let i = 0; i < checkbox.length; i++) {
-    checkbox[i].checked = isCheck;
-  }
+    for (let i = 0; i < checkbox.length; i++) {
+        checkbox[i].checked = isCheck;
+    }
+}
+
+//오름차순 내림차순
+//토글............
+
+let sortData = true;
+
+function sortToggle() {
+    if (sortData) {
+        data.sort(function (a, b) {
+            return a.age - b.age;
+        }); //오름차순
+        sortData = false;
+    } else {
+        data.sort(function (a, b) {
+            return b.age - a.age;
+        }); //내림차순
+        sortData = true;
+    }
+    setTableData(data);
 }
